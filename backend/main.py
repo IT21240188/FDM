a141849ae329c155cc66b6a7175b9b40939e44fa
Janul <it21240188@my.sliit.lk>
2024-10-05 23:50:57 +0530

    fdgj

--- backend/main.py
+++ backend/main.py
@@ -54,43 +54,17 @@ def submit():
         ordered_features = {key: features.get(key, '') for key in ordered_keys}
         # Convert features to DataFrame
         features_df = pd.DataFrame([ordered_features])
-
-        # Loop through each row in the DataFrame
-        for index, row in features_df.iterrows():
-            print(f"Row {index + 1}:")
-            
-            # Print each attribute (column) and its value
-            for column in features_df.columns:
-                value = row[column]
-                dtype = features_df[column].dtype
-                print(f"  {column}: {value} (Type: {dtype})")
-            
-            print("\n")
-
+    
         # Identify integer columns to scale
         int_columns = features_df.select_dtypes(include=['int64', 'int32', 'float64']).columns
                 
         # Scale only the integer columns
         features_df[int_columns] = scaler.transform(features_df[int_columns])
-        print("After scaling integer columns:", features_df)
-
+        
         # Apply label encoding to necessary categorical columns
         for column in features_df.columns:
             features_df[column] = object_to_int(features_df[column], column_name=column)
-        print("After label encoding:")
-        # Loop through each row in the DataFrame
-        for index, row in features_df.iterrows():
-            print(f"Row {index + 1}:")
-            
-            # Print each attribute (column), its value, and its data type
-            for column in features_df.columns:
-                value = row[column]
-                dtype = features_df[column].dtype
-                print(f"  {column}: {value} (Type: {dtype})")
-            
-            print("\n")
         
-
         # Make prediction using the pre-trained model
         prediction = Randomforest_model.predict(features_df)
         print("Prediction:", prediction)
